d2ce264faffab83df3d34a2f67b5d22d690ddec9
TsingZhang <history_zq@163.com>
2016-11-30 17:40:51 +0800

    var

--- LazyScrollView/LazyScrollView.m
+++ LazyScrollView/LazyScrollView.m
@@ -14,12 +14,13 @@
 {
     
 }
-@property (nonatomic, copy) NSDictionary *reuseViews;
-@property (nonatomic, copy) NSArray *allRects;
+@property (nonatomic, strong, nullable) NSMutableDictionary<NSString *, NSMutableArray *> *reuseViews;
+@property (nonatomic, copy, nullable) NSArray *allRects;
 @property (nonatomic, assign) NSUInteger numberOfItems;
+@property (nonatomic, strong, nullable) NSMutableDictionary<NSString *,Class> *registerClass;
 @property (nonatomic, copy) NSArray *ascendingMinEdgeRects;
 @property (nonatomic, copy) NSArray *descendingMaxEdgeRects;
-@property (nonatomic, copy) NSSet *visiableViews;
+@property (nonatomic, copy) NSMutableArray<__kindof UIView *> *visiableViews;
 @end
 
 @implementation LazyScrollView
@@ -30,6 +31,9 @@
     }
     return self;
 }
+- (void)layoutSubviews {
+    [super layoutSubviews];
+}
 - (void)reloadData {
     _numberOfItems = [_dataSource numberOfItemInScrollView:self];
     NSMutableArray *allRects = @[].mutableCopy;
@@ -37,6 +41,9 @@
         [allRects addObject:[_dataSource scrollView:self rectModelAtIndex:index]];
     }
     _allRects = allRects;
+    LSVRectModel *model = [_allRects lastObject];
+    self.contentSize = CGSizeMake(self.bounds.size.width, model.absRect.origin.y + model.absRect.size.height + 15);
+    
     if (_direction == LazyScrollViewDirectionVertical) {
         _ascendingMinEdgeRects =
         [_allRects sortedArrayUsingComparator:^NSComparisonResult(LSVRectModel*  _Nonnull obj1, LSVRectModel*  _Nonnull obj2) {
@@ -58,8 +65,41 @@
         }];
     }
 }
+
+- (void)enqueueReusableView:(UIView *)view {
+    if (!view.reuseIdentifier) {
+        return;
+    }
+    NSString *identifier = view.reuseIdentifier;
+    NSMutableArray *reuseArray = self.reuseViews[identifier];
+    if (!reuseArray) {
+        reuseArray = [NSMutableArray array];
+        [self.reuseViews setValue:reuseArray forKey:identifier];
+    }
+    [reuseArray addObject:view];
+}
 - (UIView *)dequeueReusableItemWithIdentifier:(NSString *)identifier {
-    return nil;
+    if (!identifier) {
+        return nil;
+    }
+    UIView *view = nil;
+    NSMutableArray *reuseArray = self.reuseViews[identifier];
+    if (reuseArray && reuseArray.lastObject) {
+        view = reuseArray.lastObject;
+    }
+    // 能取出view,并且veiw的标示符正确
+    if (view) {
+        // 从缓存池中获取
+        view.hidden = NO;
+        [self.reuseViews removeObjectForKey:identifier];
+        return view;
+    }
+    else {
+        Class viewClass = [self.registerClass objectForKey:identifier];
+        view = [viewClass new];
+        view.reuseIdentifier = identifier;
+        return view;
+    }
 }
 
 #pragma mark -
@@ -71,12 +111,41 @@
     CGFloat max = _direction == LazyScrollViewDirectionVertical ? self.contentOffset.y + CGRectGetHeight(self.bounds) : self.contentOffset.x + CGRectGetWidth(self.bounds);
     return max + 20;
 }
-- (NSInteger)findIndexWithMinEdge:(CGFloat)minEdge {
-
+- (NSUInteger)findIndexWithMinEdge:(CGFloat)minEdge {
+    return 0;
+}
+- (NSUInteger)findIndexWithMaxEdge:(CGFloat)maxEdge {
+    return 0;
 }
 #pragma mark -
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     
+}
+
+#pragma mark - getter
+- (NSMutableDictionary *)reuseViews {
+    if (!_reuseViews) {
+        _reuseViews = @{}.mutableCopy;
+    }
+    return _reuseViews;
+}
+- (NSArray *)allRects {
+    if (!_allRects) {
+        _allRects = @[];
+    }
+    return _allRects;
+}
+- (NSMutableDictionary *)registerClass {
+    if (!_registerClass) {
+        _registerClass = @{}.mutableCopy;
+    }
+    return _registerClass;
+}
+- (NSMutableArray *)visiableViews {
+    if (!_visiableViews) {
+        _visiableViews = @[].mutableCopy;
+    }
+    return _visiableViews;
 }
 @end
 
